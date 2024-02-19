ed574df0ce9077ca5fa89ce8fc10de89e76e1ebb
Nathan <you@example.com>
2024-02-19 19:07:36 -0300

    Add Navbar component - not finished

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import { MdMenu } from "react-icons/md";
+import { VscMenu } from "react-icons/vsc";
+import { IoMdPlay, IoIosSearch } from "react-icons/io";
+import userProfile from '../../assets/user_profile.jpg'
+import { Link } from 'react-router-dom'
 import './navbar.css'
 
 
@@ -7,7 +10,25 @@ const Navbar = () => {
   return (
     <nav>
       <div className="nav-left">
-        <h1>Logo</h1>
+        <VscMenu className='menu-icon'/>
+        <Link to='/' className='nav-link'>
+          <div className="yt-logo">
+            <IoMdPlay className='logo-triangle'/>
+          </div>
+          <h1 className='logo-name'>YouTube</h1>
+          <h3 className="logo-country">BR</h3>
+        </Link>
+      </div>
+
+      <div className="nav-center">
+        <div className="nav-input">
+          <input type="text" placeholder='Pesquisar'/>
+          <button><IoIosSearch/></button>
+        </div>
+      </div>
+
+      <div className="nav-right">
+        <img src={userProfile} />
       </div>
     </nav>
   )
