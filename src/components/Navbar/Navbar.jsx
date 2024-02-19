bb7654462970a3c8331c102d2e0bc39348f47c6e
Nathan <you@example.com>
2024-02-19 13:57:31 -0300

    Add assets

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { MdMenu } from "react-icons/md";
+import './navbar.css'
+
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav-left">
+        <h1>Logo</h1>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
