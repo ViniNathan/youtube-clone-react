120a9bf8f092f38c34f59547803e118cd616a9d5
Nathan <you@example.com>
2024-02-19 19:07:50 -0300

    Add react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Outlet } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Outlet />
     </div>
   )
 }
