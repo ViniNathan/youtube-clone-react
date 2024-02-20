6600758710e446c70ba0fe35617845fc44e180ad
Nathan <you@example.com>
2024-02-20 16:27:14 -0300

    Modifie sidebar - add link to home

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 import { GoHomeFill } from "react-icons/go";
 import { SiYoutubeshorts } from "react-icons/si";
 import { MdSubscriptions } from "react-icons/md";
+import { Link } from 'react-router-dom'
 import './sidebar.css'
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
         <div className="sidebar-top">
-            <div className="sidebar-card">
-                <GoHomeFill className='side-icon'/>
-                <h3>Início</h3>
-            </div>
+            <Link to="/" className='homepage'>
+                <div className="sidebar-card">
+                    <GoHomeFill className='side-icon'/>
+                    <h3>Início</h3>
+                </div>
+            </Link>
 
             <div className="sidebar-card">
                 <SiYoutubeshorts className='side-icon'/>
