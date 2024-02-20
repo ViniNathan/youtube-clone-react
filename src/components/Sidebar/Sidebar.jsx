4a29ad4b7fc51a226af2c6578d3862fb443f0ac4
Nathan <you@example.com>
2024-02-20 20:49:17 -0300

    Add shorts page - incomplete

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,23 +3,27 @@ import { GoHomeFill } from "react-icons/go";
 import { SiYoutubeshorts } from "react-icons/si";
 import { MdSubscriptions } from "react-icons/md";
 import { Link } from 'react-router-dom'
+import { Shorts } from '../../pages/index.js'
 import './sidebar.css'
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
         <div className="sidebar-top">
-            <Link to="/" className='homepage'>
+            <Link to="/" className='side-buttons'>
                 <div className="sidebar-card">
                     <GoHomeFill className='side-icon'/>
                     <h3>Início</h3>
                 </div>
             </Link>
 
-            <div className="sidebar-card">
-                <SiYoutubeshorts className='side-icon'/>
-                <h3>Shorts</h3>
-            </div>
+            <Link to="/shorts" className='side-buttons'>
+                <div className="sidebar-card">
+                    <SiYoutubeshorts className='side-icon'/>
+                    <h3>Shorts</h3>
+                </div>
+            </Link>
+            
             
             <div className="sidebar-card">
                 <MdSubscriptions className='side-icon'/>
