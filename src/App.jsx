82cb86dc2c00c12223b6cd5ef09af81ea0942feb
Nathan <you@example.com>
2024-02-20 08:35:37 -0300

    Add sidebar - incomplete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import Sidebar from './components/Sidebar/Sidebar'
 import { Outlet } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Sidebar />
       <Outlet />
     </div>
   )
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { GoHomeFill } from "react-icons/go";
+import { SiYoutubeshorts } from "react-icons/si";
+import { MdSubscriptions } from "react-icons/md";
+import './sidebar.css'
+
+const Sidebar = () => {
+  return (
+    <sidebar>
+        <div className="sidebar-top">
+            <div className="sidebar-card">
+                <GoHomeFill className='side-icon'/>
+                <h3>Início</h3>
+            </div>
+
+            <div className="sidebar-card">
+                <SiYoutubeshorts className='side-icon'/>
+                <h3>Shorts</h3>
+            </div>
+            
+            <div className="sidebar-card">
+                <MdSubscriptions className='side-icon'/>
+                <h3>Inscrições</h3>
+            </div>
+        </div>
+    </sidebar>
+  )
+}
+
+export default Sidebar
