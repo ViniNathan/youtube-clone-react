613a75cdeb88c5472c120de3905fff83062f37cb
Nathan <you@example.com>
2024-02-20 16:27:27 -0300

    Modifie router in main

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider,} from "react-router-dom";
+import { Home } from './pages/index.js'
 import './index.css'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />
-    // children: [
-    //   {
-    //     path: '/',
-    //     element: <Home />
-    //   },
-    //   {
-    //     path: '/about',
-    //     element: <About />
-    //   },
-    //   {
-    //     path: '/contact',
-    //     element: <Contact />
-    //   },
-    //   {
-    //     path: '/projects',
-    //     element: <Projects />
-    //   },
-    // ]
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+    ]
   },
 ]);
 
