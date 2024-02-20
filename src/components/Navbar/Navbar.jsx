8a50494d19b99f80b9674537a9a2bfe3d84db496
Nathan <you@example.com>
2024-02-20 11:52:49 -0300

    Install axios and use API to autocomplete in navbar (unfortunately only 100 requests per month)

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { VscMenu } from "react-icons/vsc";
 import { IoMdPlay, IoIosSearch } from "react-icons/io";
 import userProfile from '../../assets/user_profile.jpg'
@@ -7,6 +7,26 @@ import './navbar.css'
 
 
 const Navbar = () => {
+  const [results, setResults] = useState([]);
+  
+  const handleInputChange = async (event) => {
+    const query = event.target.value;
+    // Fazer uma chamada à API para obter as sugestões com base na query
+    try {
+      const response = await fetch(`https://youtube138.p.rapidapi.com/auto-complete/?q=${query}&hl=pt&gl=BR`, {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': '4373c0479dmshd60398751fc667cp1a0de0jsne460c0931c68',
+          'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+        }
+      });
+      const data = await response.json();
+      setResults(data.results); // Atualizar as sugestões com base nos resultados da API
+    } catch (error) {
+      console.error('Erro ao obter sugestões:', error);
+    }
+  };
+
   return (
     <nav>
       <div className="nav-left">
@@ -22,11 +42,17 @@ const Navbar = () => {
 
       <div className="nav-center">
         <div className="nav-input">
-          <input type="text" placeholder='Pesquisar'/>
+          <input type="text" placeholder='Pesquisar' onChange={handleInputChange} />
           <button><IoIosSearch/></button>
         </div>
       </div>
-
+      {results && results.length > 0 &&
+        <div className='input-suggestions'>
+          {results.map((result, index) => (
+            <button key={index} className='suggestions'><IoIosSearch className='sugg-icon'/>{result}</button>
+          ))}
+        </div>
+      };
       <div className="nav-right">
         <img src={userProfile} />
       </div>
