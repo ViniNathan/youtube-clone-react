0689528d5523c1c6fc4b6168e1473d9e1eae414c
Nathan <you@example.com>
2024-02-20 21:18:02 -0300

    Add some shorts features

--- src/pages/shorts/Shorts.jsx
+++ src/pages/shorts/Shorts.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { BiSolidLike , BiSolidDislike} from "react-icons/bi";
+import { MdComment } from "react-icons/md";
+
 import './shorts.css'
 
 const Shorts = () => {
   return (
-    <div>Shorts</div>
+    <div className="shorts-grid">
+      <div className="shorts-card"></div>
+      <div className="shorts-button like"><BiSolidLike/></div>
+      <div className="shorts-button dislike"><BiSolidDislike/></div>
+      <div className="shorts-button comment"><MdComment/></div>
+
+    </div>
   )
 }
 
