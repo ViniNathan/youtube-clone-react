9a51a719081b33dae1934023e523f28b1992e342
Nathan <you@example.com>
2024-02-21 09:57:57 -0300

    Add details in video page - not finished

--- src/pages/Video/Video.jsx
+++ src/pages/Video/Video.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import user_profile from '../../assets/user_profile.jpg'
+import { BiLike , BiDislike} from "react-icons/bi";
+import { PiShareFatLight } from "react-icons/pi";
 import './video.css'
 
 const Video = () => {
   return (
-    <div>Video</div>
+    <div className="videopage-grid">
+      <div className="video-screen"></div>
+      <div className="video-title">Título do vídeo</div>
+      <div className="video-channel"><img src={user_profile} alt="" /></div>
+      <div className="channel-details">
+        <h3>Nome do canal</h3>
+        <p>Inscritos</p>
+      </div>
+      <button className='sub-btn'>Inscrever-se</button>
+      <div className="btn-container">
+      <button className='like'><BiLike/><p>Likes</p></button>
+      <div className="separator"></div>
+      <button className='dislike'><BiDislike/></button>
+      </div>
+      <button className='share'><PiShareFatLight className='share-icon'/><p>Compartilhar</p></button>
+
+      <div className="related-videos">
+        <div className="videos-card"></div>
+      </div>
+
+      <div className="comments-section">
+        
+      </div>
+    </div>
   )
 }
 
