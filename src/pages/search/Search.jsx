6c994a6d40df10d9220a4bf243960228ab146f04
Nathan <you@example.com>
2024-02-23 10:47:07 -0300

    Add api functions to search page

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-import Sidebar from '../../components/Sidebar/Sidebar'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchSearch } from '../../utils/videoServiceApi'
 import SearchVideoCard from '../../components/SearchVideoCard/SearchVideoCard'
+import Sidebar from '../../components/Sidebar/Sidebar'
 import './search.css'
 
 const Search = () => {
+  const [searchDetails, setSearchDetails] = useState(null);
+  const { query } = useParams(); // Obtém a query da URL
+
+  useEffect(() => {
+    const getSearchData = async () => {
+      try {
+        const searchData = await fetchSearch(query);
+        setSearchDetails(searchData);
+      } catch (error) {
+        console.error('Erro ao buscar os detalhes da pesquisa:', error);
+      }
+    };
+
+    getSearchData();
+  }, [query]);
+
+
   return (
     <div>
         <Sidebar/>
--- src/utils/videoServiceApi.js
+++ src/utils/videoServiceApi.js
@@ -39,3 +39,28 @@ export const fetchChannelDetails = async (channelId) => {
   }
 };
 
+
+export const fetchSearch = async (query) => {
+  const options = {
+    method: 'GET',
+    url: `https://yt-api.p.rapidapi.com/search?query=${query}`,
+    params: {
+      geo: 'BR',
+      lang: 'pt'
+    },
+    headers: {
+      'X-RapidAPI-Key': '4373c0479dmshd60398751fc667cp1a0de0jsne460c0931c68',
+      'X-RapidAPI-Host': 'yt-api.p.rapidapi.com'
+    }
+  };
+
+  try {
+    const response = await axios.request(options);
+    return response.data;
+  }
+  catch (error) {
+    console.error('Erro ao realizar a pesquisa:', error);
+    throw error;
+  }
+};
+
