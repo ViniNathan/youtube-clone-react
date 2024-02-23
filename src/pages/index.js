a81ec79060802b9a407bec0ffebb1d6dcaed2070
Nathan <you@example.com>
2024-02-23 09:49:24 -0300

    Add search page export

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as Home } from './home/Home';
 export { default as Shorts } from './shorts/Shorts';
 export { default as Video } from './video/Video';
+export { default as Search } from './search/Search';
