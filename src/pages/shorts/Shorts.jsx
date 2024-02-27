60a5f84b72c6e7dc6ebc4dd8e655a8631f1ee82f
Nathan <you@example.com>
2024-02-27 14:41:56 -0300

    Add shorts functions

--- src/pages/shorts/Shorts.jsx
+++ src/pages/shorts/Shorts.jsx
@@ -1,25 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { fetchShorts, fetchNextShorts } from '../../utils/videoServiceApi'
 import Sidebar from '../../components/Sidebar/Sidebar';
 import { BiSolidLike , BiSolidDislike} from "react-icons/bi";
 import { MdComment } from "react-icons/md";
-import user_profile from '../../assets/user_profile.jpg'
-
 import './shorts.css'
 
 const Shorts = () => {
+  const [shortsDetails, setShortsDetails] = useState(null);
+  const [nextShortsDetails, setNextShortsDetails] = useState(null);
+  const path = window.location.pathname;
+  const query = path.split('/').pop();
+
+
+  useEffect(() => {
+    const getShortsData = async () => {
+      try {
+        const shortsData = await fetchShorts(query);
+        setShortsDetails(shortsData);
+
+        const nextShortsData = await fetchNextShorts(query);
+        setNextShortsDetails(nextShortsData);
+        console.log(nextShortsData.data);
+      } catch (error) {
+        console.error('Erro ao buscar os detalhes do shorts:', error);
+      }
+    };
+
+    getShortsData();
+  }, [query]);
+
   return (
     <div>
       <Sidebar/>
-      <div className="shorts-grid">
-      <div className="shorts-card"></div>
-      <div className="shorts-button like"><BiSolidLike/></div>
-      <div className="shorts-button dislike"><BiSolidDislike/></div>
-      <div className="shorts-button comment"><MdComment/></div>
-      <div className="channel-imgout"><img src={user_profile} alt="" /></div>
-      <div className="channel-imgin"><img src={user_profile} alt="" /></div>
-      <div className="shorts-channel-name">Nome do canal</div>
-      <div className="shorts-title">Título</div>
-    </div>
+      {shortsDetails && (
+        <div className="shorts-grid">
+          <div className="shorts-card">
+          <iframe width="315" height="560" src={`https://www.youtube.com/embed/${shortsDetails.videoId}?rel=0&autoplay=1&controls=0`} frameborder="0"></iframe>
+          </div>
+          <div className="shorts-button s-like"><BiSolidLike/></div>
+          <div className="shorts-button s-dislike"><BiSolidDislike/></div>
+          <div className="shorts-button comment"><MdComment/></div>
+          <img src={shortsDetails.channelThumbnail[2].url} alt="" className="channel-imgout"/>
+          <div className="shorts-details">
+            <div className="shorts-channel-details">
+              <img src={shortsDetails.channelThumbnail[2].url} alt="" className='channel-imgin'/>
+              <h3>{shortsDetails.channelTitle}</h3>
+            </div>
+            <p>{shortsDetails.title}</p>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
--- src/utils/videoServiceApi.js
+++ src/utils/videoServiceApi.js
@@ -120,4 +120,50 @@ export const fetchComments = async (videoId) => {
   }
 };
 
+export const fetchShorts = async (shortsId) => {
+  const options = {
+    method: 'GET',
+    url: `https://yt-api.p.rapidapi.com/shorts/info?id=${shortsId}`,
+    params: {
+      geo: 'BR',
+      lang: 'pt'
+    },
+    headers: {
+      'X-RapidAPI-Key': '4373c0479dmshd60398751fc667cp1a0de0jsne460c0931c68',
+      'X-RapidAPI-Host': 'yt-api.p.rapidapi.com'
+    }
+  };
 
+  try {
+    const response = await axios.request(options);
+    return response.data;
+  }
+  catch (error) {
+    console.error('Erro ao realizar a pesquisa de shorts:', error);
+    throw error;
+  }
+};
+
+export const fetchNextShorts = async (shortsId) => {
+  const options = {
+    method: 'GET',
+    url: `https://yt-api.p.rapidapi.com/shorts/sequence?id=${shortsId}`,
+    params: {
+      geo: 'BR',
+      lang: 'pt'
+    },
+    headers: {
+      'X-RapidAPI-Key': '4373c0479dmshd60398751fc667cp1a0de0jsne460c0931c68',
+      'X-RapidAPI-Host': 'yt-api.p.rapidapi.com'
+    }
+  };
+
+  try {
+    const response = await axios.request(options);
+    return response.data;
+  }
+  catch (error) {
+    console.error('Erro ao realizar a pesquisa de shorts:', error);
+    throw error;
+  }
+};
