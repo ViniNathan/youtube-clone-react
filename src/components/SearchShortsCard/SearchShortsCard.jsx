7d4dc897ddff76b20fc015de88dbb1079e851e47
Nathan <you@example.com>
2024-02-27 17:20:40 -0300

    Add truncateString to shorts title

--- src/components/SearchShortsCard/SearchShortsCard.jsx
+++ src/components/SearchShortsCard/SearchShortsCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { truncateString } from '../../utils/functions'
 import './searchShortsCard.css'
 
 const SearchShortsCards = ({thumbnail, title, views, shortsId}) => {
@@ -7,7 +8,7 @@ const SearchShortsCards = ({thumbnail, title, views, shortsId}) => {
     <Link to ={`../shorts/${shortsId}`} className="search-shorts-card">
         <div className="search-shorts-thumbnail"><img src={thumbnail} alt="thumbnail" /></div>
         <div className="search-shorts-details">
-            <p className='search-shorts-card-title'>{title}</p>
+            <p className='search-shorts-card-title'>{truncateString(title, 6)}</p>
             <p>{views}</p>
         </div>
     </Link>
