2929589d85ddbd762c97768ea9384daa0bab54ab
Nathan <you@example.com>
2024-02-27 09:02:27 -0300

    Add path to shorts

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
         element: <Home />
       },
       {
-        path: '/shorts',
+        path: '/shorts/:shortsId',
         element: <Shorts />
       },
       {
