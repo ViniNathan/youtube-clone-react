85f8a703a31296b49953344bd3e1f0c3f78ef7e9
Nathan <viniciosnathan@gmail.com>
2024-03-05 08:30:14 -0300

    Add functionality to shorts button

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import { GoHomeFill } from "react-icons/go";
 import { SiYoutubeshorts } from "react-icons/si";
 import { MdSubscriptions } from "react-icons/md";
+import { fetchTrending } from '../../utils/home-api'
+import { removeDuplicateVideos } from '../../utils/functions';
 import { Link } from 'react-router-dom'
 import './sidebar.css'
 
 const Sidebar = () => {
+  const [shortsVideo, setShortsVideo] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchTrending();
+      // Filtra os vídeos para garantir chaves únicas
+      const uniqueVideos = removeDuplicateVideos(data.data);
+      const shortsVideo = uniqueVideos.find(video => video.type === "shorts_listing");
+      if (shortsVideo) {
+        setShortsVideo(shortsVideo);
+      }
+    };
+    fetchData();
+  }, []);
+
+  // Gerar um número aleatório entre 0 e 10
+  const randomIndex = Math.floor(Math.random() * 10);
+
   return (
     <div className='sidebar'>
         <div className="sidebar-top">
@@ -16,13 +36,14 @@ const Sidebar = () => {
                 </div>
             </Link>
 
-            <Link to="/shorts" className='side-buttons'>
-                <div className="sidebar-card">
-                    <SiYoutubeshorts className='side-icon'/>
-                    <h3>Shorts</h3>
-                </div>
-            </Link>
-            
+            {shortsVideo && (
+              <Link to={`shorts/${shortsVideo.data[randomIndex].videoId}`} className='side-buttons'>
+                  <div className="sidebar-card">
+                      <SiYoutubeshorts className='side-icon'/>
+                      <h3>Shorts</h3>
+                  </div>
+              </Link>
+            )}
             
             <div className="sidebar-card">
                 <MdSubscriptions className='side-icon'/>
@@ -33,4 +54,4 @@ const Sidebar = () => {
   )
 }
 
-export default Sidebar
+export default Sidebar;
